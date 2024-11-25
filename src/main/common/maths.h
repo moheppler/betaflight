36c48fffed9e065f213508d9c502cd42fd923e5d
moheppler <mheppler1204@gmail.com>
2024-11-25 17:28:50 +0100

    added minimum function for doubles

--- src/main/common/maths.h
+++ src/main/common/maths.h
@@ -168,3 +168,12 @@ static inline double constraind(double amt, double low, double high)
     else
         return amt;
 }
+
+static inline double mind(double val1, double val2)
+{
+    if (val1 < val2) {
+        return val1;
+    } else {
+        return val2;
+    }
+}
